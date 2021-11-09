233fe48c1f4e76a3c33429db487321fa78432081
Jon <jon.cy.lee98@gmail.com>
2021-11-08 19:03:08 -0500

    ignore unreachable torrents when looking for -r

--- qbit_manage.py
+++ qbit_manage.py
@@ -383,7 +383,7 @@ def rem_unregistered():
                                 f'\n - Status: {msg_up} '
                                 f'\n - Tracker: {t_url} '
                                 f'\n - Deleted .torrent AND content files.')
-                    if (x.status == 4 and 'DOWN' not in msg_up):
+                    if (x.status == 4 and ('DOWN' not in msg_up or 'UNREACHABLE' not in msg_up)):
                         pot_unr += (f'\n - Torrent: {torrent.name}')
                         pot_unr += (f'\n     - Message: {x.msg}')
                     if ('UNREGISTERED' in msg_up or \
@@ -393,7 +393,7 @@ def rem_unregistered():
                         'NOT EXIST' in msg_up or \
                         'UNKNOWN TORRENT' in msg_up or \
                         'PACKS ARE AVAILABLE' in msg_up \
-                        ) and x.status == 4 and 'down' not in msg_up:
+                        ) and x.status == 4 and ('DOWN' not in msg_up or 'UNREACHABLE' not in msg_up):
                         logger.debug(f'Torrent counts: {t_count}')
                         logger.debug(f'msg: {t_msg}')
                         logger.debug(f'status: {t_status}')
