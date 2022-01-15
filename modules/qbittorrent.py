5fed8cd0e8b2458a05174bb754382b7676485172
bobokun <jon.cy.lee98@gmail.com>
2022-01-15 11:33:08 -0500

    better description of error #97 QBittorrent Version #97

--- modules/qbittorrent.py
+++ modules/qbittorrent.py
@@ -29,9 +29,10 @@ class Qbt:
             logger.debug(f'qBittorrent Web API: {self.client.app.web_api_version}')
             logger.debug(f'qbit_manage support version: {SUPPORTED_VERSION}')
             if not Version.is_app_version_supported(CURRENT_VERSION):
-                e = f"Qbittorrent Error: qbit_manage is only comaptible with {SUPPORTED_VERSION} or lower. You are currently on {CURRENT_VERSION}"
+                e = (f"Qbittorrent Error: qbit_manage is only comaptible with {SUPPORTED_VERSION} or lower. You are currently on {CURRENT_VERSION}." + '\n'
+                     + f"Please downgrade to your Qbittorrent version to {SUPPORTED_VERSION} to use qbit_manage.")
                 self.config.notify(e, "Qbittorrent")
-                print_line(e, 'CRITICAL')
+                print_multiline(e, 'CRITICAL')
                 sys.exit(0)
             logger.info("Qbt Connection Successful")
         except LoginFailed:
