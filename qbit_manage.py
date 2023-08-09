084616ad8345b0a507bcdaad04fcb80c8e993bab
buthed010203 <naguiar010203@gmail.com>
2023-08-09 12:02:35 -0400

    Update qbit_manage.py

--- qbit_manage.py
+++ qbit_manage.py
@@ -536,6 +536,8 @@ def start():
             stats["deleted"] += share_limits.stats_deleted
             stats["deleted_contents"] += share_limits.stats_deleted_contents
             stats["cleaned_share_limits"] += share_limits.stats_deleted + share_limits.stats_deleted_contents
+            if share_limits.stats_deleted_contents > 0:
+                time.sleep(3)  # For some reason remove orphaned will try (and fail) to remove the files we just deleted if we don't sleep here
 
         # Remove Orphaned Files
         if cfg.commands["rem_orphaned"]:
