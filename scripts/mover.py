34c069a3e9a35347e40d273a1ee92ba58f7e50a7
denys.kozhevnikov <github@mail.noonamer.com>
2024-03-26 10:01:27 +0000

    Add direct file mover

--- scripts/mover.py
+++ scripts/mover.py
@@ -23,6 +23,7 @@ parser.add_argument(
     "--days-from", "--days_from", help="Set Number of Days to stop torrents between two offsets", type=int, default=0
 )
 parser.add_argument("--days-to", "--days_to", help="Set Number of Days to stop torrents between two offsets", type=int, default=2)
+parser.add_argument("--debug", help="Enable debug logger for mover", default=False)
 # --DEFINE VARIABLES--#
 
 # --START SCRIPT--#
@@ -34,22 +35,28 @@ except ModuleNotFoundError:
     print('Requirements Error: qbittorrent-api not installed. Please install using the command "pip install qbittorrent-api"')
     sys.exit(1)
 
-
 def filter_torrents(torrent_list, timeoffset_from, timeoffset_to, cache_mount):
     result = []
     for torrent in torrent_list:
         if torrent.added_on >= timeoffset_to and torrent.added_on <= timeoffset_from:
-            if not cache_mount or exists_in_cache(cache_mount, torrent.content_path):
+            if not cache_mount or os.path.exists(cache_path(cache_mount, torrent.content_path)):
                 result.append(torrent)
         elif torrent.added_on < timeoffset_to:
             break
     return result
 
+def cache_path(cache_mount, content_path):
+    return os.path.join(cache_mount, content_path.lstrip("/"))
 
-def exists_in_cache(cache_mount, content_path):
-    cache_path = os.path.join(cache_mount, content_path.lstrip("/"))
-    return os.path.exists(cache_path)
-
+def find_hardlinks(file_path, cache_mount):
+    inode = os.stat(file_path).st_ino
+    hardlinks = set()
+    for dirpath, _, filenames in os.walk(cache_mount):
+        for filename in filenames:
+            candidate_path = os.path.join(dirpath, filename)
+            if os.stat(candidate_path).st_ino == inode:
+                hardlinks.add(candidate_path)
+    return hardlinks
 
 def stop_start_torrents(torrent_list, pause=True):
     for torrent in torrent_list:
@@ -59,7 +66,6 @@ def stop_start_torrents(torrent_list, pause=True):
         else:
             print(f"Resuming: {torrent.name} [{torrent.added_on}]")
             torrent.resume()
-
 
 if __name__ == "__main__":
     current = datetime.now()
@@ -83,15 +89,32 @@ if __name__ == "__main__":
 
     torrents = filter_torrents(torrent_list, timeoffset_from.timestamp(), timeoffset_to.timestamp(), args.cache_mount)
 
+    if not torrents:
+        print(f"Nothing to move within {args.days_from} - {args.days_to} days ago. Exiting...")
+        sys.exit(0)
+
     # Pause Torrents
     print(f"Pausing [{len(torrents)}] torrents from {args.days_from} - {args.days_to} days ago")
     stop_start_torrents(torrents, True)
     time.sleep(10)
+    
+    file_paths = set()
+    for torrent in torrents:
+        file_path = cache_path(args.cache_mount, torrent.content_path)
+        file_paths.add(file_path)
+        
+        for link in find_hardlinks(file_path, args.cache_mount):
+            file_paths.add(link)
+    
+    folder = "/tmp/qbitmover"
+    os.makedirs(folder, exist_ok=True)
+    
+    tmp_file = f"{folder}/movelist_{current.strftime('%Y-%m-%d_%H-%M-%S')}.list"
+    with open(tmp_file, "w") as file:
+         file.writelines(line + "\n" for line in file_paths)
     # Start mover
     print("Starting Mover")
-    # Or using mover tunning
-    # os.system('/usr/local/sbin/mover start')
-    os.system("/usr/local/sbin/mover.old start")
+    os.system(f"cat {tmp_file} | /usr/local/sbin/move -d {int(args.debug)}")
     # Start Torrents
     print(f"Resuming [{len(torrents)}] paused torrents from {args.days_from} - {args.days_to} days ago")
     stop_start_torrents(torrents, False)
