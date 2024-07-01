bde9d8a23486bc8b02cb43a6bd4203e4779cacde
Tony <5747393+convexshiba@users.noreply.github.com>
2024-07-01 14:22:12 -0700

    Update mover.py: add optional arg --status-filter to allow fine tune which torrents to pauseUpdate mover.py: add optional arg --status-filter to allow fine tune which torrents to pause (#599)
    * Update mover.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update mover.py: making the default behavior to only move `completed` torrents
    
    This will leave 1) unfinished files 2) actively seeding files in `downloading` torrents in cache. This helps to keep write-heavy operation on Cache, not on hard drive.
    
    Change this to "all" if you want this to always move everything every time.
    
    * minor fixes in help description
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: bobokun <12660469+bobokun@users.noreply.github.com>

--- scripts/mover.py
+++ scripts/mover.py
@@ -38,6 +38,29 @@ parser.add_argument(
     action="store_true",
     default=False,
 )
+parser.add_argument(
+    "--status-filter",
+    help="Define a status to limit which torrents to pause. Useful if you want to leave certain torrents unpaused.",
+    choices=[
+        "all",
+        "downloading",
+        "seeding",
+        "completed",
+        "paused",
+        "stopped",
+        "active",
+        "inactive",
+        "resumed",
+        "running",
+        "stalled",
+        "stalled_uploading",
+        "stalled_downloading",
+        "checking",
+        "moving",
+        "errored",
+    ],
+    default="completed",
+)
 # --DEFINE VARIABLES--#
 
 # --START SCRIPT--#
@@ -96,7 +119,7 @@ if __name__ == "__main__":
 
     timeoffset_from = current - timedelta(days=args.days_from)
     timeoffset_to = current - timedelta(days=args.days_to)
-    torrent_list = client.torrents.info(sort="added_on", reverse=True)
+    torrent_list = client.torrents.info(status_filter=args.status_filter, sort="added_on", reverse=True)
 
     torrents = filter_torrents(torrent_list, timeoffset_from.timestamp(), timeoffset_to.timestamp(), args.cache_mount)
 
