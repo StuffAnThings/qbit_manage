5751b9a18a2182e7a5c253fd0fa70f899b8f0736
bobokun <jon.cy.lee98@gmail.com>
2025-05-05 09:12:23 -0400

    fix github actions

--- scripts/pre-commit/increase_version.sh
+++ scripts/pre-commit/increase_version.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-# Check if there are any changes staged for commit
-if [[ -z $(git diff --cached --name-only) ]]; then
-  echo "There are no changes staged for commit. Skipping version update."
-  exit 0
-fi
-
-# Check if the VERSION file is staged for modification
-if git diff --cached --name-only | grep -q "VERSION"; then
-  echo "The VERSION file is already modified. Skipping version update."
-  exit 0
-elif git diff --name-only | grep -q "VERSION"; then
-  echo "The VERSION file has unstaged changes. Please stage them before committing."
-  exit 0
-elif ! git show --name-only HEAD | grep -q "VERSION"; then
-  update_version_if_develop
-fi
-
 update_version_if_develop() {
   # Read the current version from the VERSION file
   current_version=$(<VERSION)
@@ -42,3 +25,20 @@ update_version_if_develop() {
     exit 0
   fi
 }
+
+# Check if there are any changes staged for commit
+if [[ -z $(git diff --cached --name-only) ]]; then
+  echo "There are no changes staged for commit. Skipping version update."
+  exit 0
+fi
+
+# Check if the VERSION file is staged for modification
+if git diff --cached --name-only | grep -q "VERSION"; then
+  echo "The VERSION file is already modified. Skipping version update."
+  exit 0
+elif git diff --name-only | grep -q "VERSION"; then
+  echo "The VERSION file has unstaged changes. Please stage them before committing."
+  exit 0
+elif ! git show --name-only HEAD | grep -q "VERSION"; then
+  update_version_if_develop
+fi
